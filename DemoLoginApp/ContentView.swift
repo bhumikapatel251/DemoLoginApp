e0e04a1d873de4e7d95ef0852437bd3d3f37b2f7
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-06-10 14:50:16 +0530

    Navigatinview and scrollview create

--- DemoLoginApp/ContentView.swift
+++ DemoLoginApp/ContentView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        NavigationView{
+            ScrollView{
+                Text("Here is my creation Account Screen")
+            }
+        }
+        Text("Lets build out our login screen")
             .padding()
     }
 }
