a76f6719d2517760ed021f3973876d91588a67f7
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-06-10 12:14:41 +0530

    Initial Commit

---
+++ DemoLoginApp/DemoLoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoLoginAppApp.swift
+//  DemoLoginApp
+//
+//  Created by Bhumika Patel on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoLoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
